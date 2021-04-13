d1e36a44d54d075b097143ef4652b62162e4a084
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-13 13:28:48 -0700

    fix bugs

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express()
 app.use('/graphql', expressGraphQL({
     graphiql: true
 }))
-app.listen(5000., () => console.log('Server Running'))
+app.listen(5000, () => console.log('Server Running'))
